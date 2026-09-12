8fe50bc033a0d771e25d7bfaa33ad222ffd73c76
Jom3nd <joaomendescruz870@gmail.com>
2026-09-12 20:36:19 -0300

    feat: adiciona a variável server para início do servidor

--- index.js
+++ index.js
@@ -6,8 +6,7 @@ const PORT = process.env.PORT || 3000;
     app.get('/', (req, res) => {
     res.send('Docker exercise is working fine!');
 })
-
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
     console.log(`Server is running on "http://localhost:${PORT}"`);
 })
 server.on('error', (err) => {
